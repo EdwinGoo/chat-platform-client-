0ab3317674100c961efdf4edf1fc2ddfb221d373
EdwinGoo <egoomoy@gmail.com>
2022-08-31 00:36:33 +0900

    select box 만들어봄

---
+++ src/components/common/SelectBox.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import useSelect from "../../hooks/useSelect";
+import palette from "../../assets/palette";
+
+const SelectBoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Label = styled.label`
+  position: relative;
+  display: inline-block;
+  width: ${({ width }) => `${width}rem`};
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 0.5rem; /* 사이즈 */
+    height: 0.5rem; /* 사이즈 */
+    border-top: 3px solid ${palette.gray[5]}; /* 선 두께 */
+    border-right: 3px solid ${palette.gray[5]}; /* 선 두께 */
+    border-radius: 0.125rem;
+    cursor: pointer;
+    transform: rotate(135deg); /* 각도 */
+    top: 13px; /* 기본 0px 값으로 해주세요 */
+    right: 20px;
+  }
+`;
+
+const Select = styled.select`
+  padding: 10px 10px;
+  font-size: 1rem;
+  width: 100%;
+  border-radius: 4px;
+  appearance: none;
+  border: solid 2px ${palette.gray[4]};
+
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  font-weight: 700;
+`;
+
+const SelectItemWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 4px;
+  margin-top: 0.3rem;
+  width: ${({ width }) => `${width}rem`};
+  padding: 0.1rem 0 0.1rem 0;
+  border: solid 2px ${palette.gray[4]};
+  background-color: white;
+  box-sizing: border-box;
+`;
+const SelectItem = styled.div`
+  font-size: 1rem;
+  text-align: left;
+  font-weight: 400;
+  padding: 0.5rem;
+  &:hover {
+    background-color: ${palette.gray[2]};
+    cursor: pointer;
+  }
+`;
+
+function SelectBox({ options, selectedOption, setSelectedOption, width }) {
+  const labelRef = useRef(null);
+  const [clickSelectedBox, setClickSelectedBox] = useSelect(labelRef);
+  useEffect(() => {}, [clickSelectedBox]);
+
+  const handleOpenSelectBox = (e) => {
+    e.preventDefault();
+    setClickSelectedBox(true);
+  };
+  const handleSelectBox = (e, option) => {
+    const optionValue = option ? option : e.target.value;
+    setSelectedOption(optionValue);
+    setClickSelectedBox(false);
+  };
+
+  return (
+    <>
+      <SelectBoxWrapper>
+        <Label ref={labelRef} width={width} onMouseDown={handleOpenSelectBox}>
+          <Select value={selectedOption} onChange={handleSelectBox}>
+            {options.map((o) => {
+              return (
+                <option key={o} value={o}>
+                  {o}
+                </option>
+              );
+            })}
+          </Select>
+          {clickSelectedBox && (
+            <SelectItemWrapper width={width}>
+              {options.map((option) => (
+                <SelectItem
+                  width={width}
+                  key={option}
+                  value={option}
+                  onClick={(e) => handleSelectBox(e, option)}
+                  // isSelected={option === selectedOption}
+                >
+                  {option}
+                </SelectItem>
+              ))}
+            </SelectItemWrapper>
+          )}
+        </Label>
+      </SelectBoxWrapper>
+    </>
+  );
+}
+
+export default SelectBox;
